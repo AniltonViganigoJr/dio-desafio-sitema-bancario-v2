392167520c87b2b03231f0736a57652abf5c4c21
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2025-12-09 00:54:47 -0300

    feat: entidade core criada arquivo (core.py)

---
+++ entidades/core.py
@@ -0,0 +1,36 @@
+from menu import menu
+from operacoes import *
+
+
+
+def core():
+    clientes = []
+    contas = []
+
+    while True:
+        opcao = menu()
+
+        if opcao == "d":
+            depositar(clientes)
+
+        elif opcao == "s":
+            sacar(clientes)
+
+        elif opcao == "e":
+            exibir_extrato(clientes)
+
+        elif opcao == "nu":
+            criar_cliente(clientes)
+
+        elif opcao == "nc":
+            numero_conta = len(contas) + 1
+            criar_conta(numero_conta, clientes, contas)
+
+        elif opcao == "lc":
+            listar_contas(contas)
+
+        elif opcao == "q":
+            break
+
+        else:
+            print("\n@@@ Operação inválida, por favor selecione novamente a operação desejada. @@@")
